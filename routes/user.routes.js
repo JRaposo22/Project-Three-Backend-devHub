326fd12591a1beb4d95aeb71f5e53caa852fedc6
Joao <raposo2281@gmail.com>
2023-03-15 16:45:46 +0000

    adds update profile

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -20,21 +20,30 @@ router.get('/profile/:id' , async (req, res, next) => {
    
 })
 
-// edit profile
 
-router.put('/profile/:id', fileUploader.single('imageUrl'), async (req, res, next) => {
-    const {username, email, imageUrl} = req.body;
-    console.log(req.body)
+
+//upload file
+router.post('/upload', fileUploader.single('imageUrl'), async (req, res, next) => {
     
-    //let imageUrl
 
-    //if(!req.file) imageUrl = req.file.path;
-    //else imageUrl = currentImage
+   if(!req.file){
+    next(new Error('No file upload'));
+    return;
+   }
 
+   res.json({fileUrl : req.file.path})
+})
+
+// edit profile
+router.put('/profile/:id', async (req, res, next) => {
+    const {username, email, imageUrl} = req.body;
+    const {id} = req.params;
+    console.log(imageUrl)
+    
     try {
         const updatedProfile = await User.findByIdAndUpdate(
             id, {username, email, imageUrl}, { new: true });
-
+            console.log(updatedProfile)
         res.json({updatedProfile});
     } catch (error) {
         res.json(error);
